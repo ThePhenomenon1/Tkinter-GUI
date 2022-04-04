c9bc2fffbe80bc0d63012b634fc77fae6485a379
PHILLIP EDWIN MUGISHA <pedmugisha@gmail.com>
2022-04-04 07:31:18 +0300

    Add files via upload

---
+++ tk_zen.py.py
@@ -0,0 +1,30 @@
+
+# Let's take a look at the example Python GUI application that can be created with the use of the built-in tkinter module. The following GUI application displays a single Python Zen button. 
+
+# When the button is clicked, the application will open a new window containing the Zen of Python text that was imported from the this module. The this module is a Python easter egg. After import, it prints on standard output the 19 aphorisms that are the guiding principles of Python's design.
+
+# Event-driven programming in GUIs
+import this
+from tkinter import Tk, Frame, Button, LEFT, messagebox
+
+rot13 = str.maketrans(
+    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
+    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm",
+)
+
+def main_window(root: Tk):
+    frame = Frame(root)
+    frame.pack()
+
+    zen_button = Button(frame, text="Python Zen", command=show_zen)
+    zen_button.pack(side=LEFT)
+
+def show_zen():
+    messagebox.showinfo("Zen of Python", this.s.translate(rot13))
+
+if __name__ == "__main__":
+    root = Tk()
+    main_window(root)
+    root.mainloop()
+
+
