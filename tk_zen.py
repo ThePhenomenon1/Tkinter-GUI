cc1cf2ed920b99297b8e1218e7818171c7384daa
PHILLIP EDWIN MUGISHA <pedmugisha@gmail.com>
2022-04-04 07:37:50 +0300

    Update tk_zen.py

--- tk_zen.py
+++ tk_zen.py
@@ -1,7 +1,7 @@
 
-# Let's take a look at the example Python GUI application that can be created with the use of the built-in tkinter module. 
-# The following GUI application displays a single Python Zen button. 
+# Python GUI application that can be created with the use of the built-in tkinter module. 
 
+# This GUI application displays a single Python Zen button. 
 # When the button is clicked, the application will open a new window containing the Zen of Python text that was imported from the this module. 
 # The this module is a Python easter egg. 
 # After import, it prints on standard output the 19 aphorisms that are the guiding principles of Python's design.
